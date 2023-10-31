be4ec0fb1df490651cc665a08feab54036032056
chengzao <czhlink@163.com>
2023-11-01 00:27:42 +0800

    refactor(vite config): update vite config

--- packages/vitePack/vite.config.ts
+++ packages/vitePack/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import fastGlob from 'fast-glob';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-const entries = fastGlob.sync('src/**/*.{ts,tsx}');
 import dts from 'vite-plugin-dts';
 import { libInjectCss, scanEntries } from 'vite-plugin-lib-inject-css';
+// const entries = fastGlob.sync('src/**/*.{ts,tsx}');
 
 export default defineConfig(async ({ command, mode }) => {
   return {
@@ -14,41 +14,72 @@ export default defineConfig(async ({ command, mode }) => {
       dts({
         outDir: 'dist/esm',
       }),
-      libInjectCss(),
+      libInjectCss({
+        formats: ['es', 'cjs'],
+        entry: scanEntries('src'),
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          input: Object.fromEntries(
+            fastGlob
+              .sync('src/**/*.{ts,tsx}')
+              .map((file) => [
+                path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+                fileURLToPath(new URL(file, import.meta.url)),
+              ]),
+          ),
+          output: [
+            {
+              dir: 'dist/esm',
+              format: 'es',
+              entryFileNames: '[name].js',
+              // exports: 'named',
+              // preserveModules: true,
+              // preserveModulesRoot: 'src',
+            },
+            {
+              dir: 'dist/cjs',
+              format: 'cjs',
+              exports: 'named',
+              // preserveModules: true,
+              // preserveModulesRoot: 'src',
+            },
+          ],
+        },
+      }),
     ],
-    build: {
-      rollupOptions: {
-        external: ['react', 'react-dom'],
-        input: Object.fromEntries(
-          fastGlob
-            .sync('src/**/*.{ts,tsx}')
-            .map((file) => [
-              path.relative('src', file.slice(0, file.length - path.extname(file).length)),
-              fileURLToPath(new URL(file, import.meta.url)),
-            ]),
-        ),
-        output: [
-          {
-            dir: 'dist/esm',
-            format: 'es',
-            entryFileNames: '[name].js',
-            // exports: 'named',
-            // preserveModules: true,
-            // preserveModulesRoot: 'src',
-          },
-          {
-            dir: 'dist/cjs',
-            format: 'cjs',
-            exports: 'named',
-            // preserveModules: true,
-            // preserveModulesRoot: 'src',
-          },
-        ],
-      },
-      lib: {
-        entry: entries,
-        // formats: ['es', 'cjs'],
-      },
-    },
+    // build: {
+    //   rollupOptions: {
+    //     external: ['react', 'react-dom'],
+    //     input: Object.fromEntries(
+    //       fastGlob
+    //         .sync('src/**/*.{ts,tsx}')
+    //         .map((file) => [
+    //           path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+    //           fileURLToPath(new URL(file, import.meta.url)),
+    //         ]),
+    //     ),
+    //     output: [
+    //       {
+    //         dir: 'dist/esm',
+    //         format: 'es',
+    //         entryFileNames: '[name].js',
+    //         // exports: 'named',
+    //         // preserveModules: true,
+    //         // preserveModulesRoot: 'src',
+    //       },
+    //       {
+    //         dir: 'dist/cjs',
+    //         format: 'cjs',
+    //         exports: 'named',
+    //         // preserveModules: true,
+    //         // preserveModulesRoot: 'src',
+    //       },
+    //     ],
+    //   },
+    //   lib: {
+    //     entry: entries,
+    //     // formats: ['es', 'cjs'],
+    //   },
+    // },
   };
 });
