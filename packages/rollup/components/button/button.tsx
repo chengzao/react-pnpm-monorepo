90646bcb0c88da2f8aa9cc9bb111cc8d6c9d17b7
chengzao <czhlink@163.com>
2023-12-08 15:31:22 +0800

    feat(rollup): add rollup build folder

---
+++ packages/rollup/components/button/button.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { isNumeric } from '../util';
+
+interface ButtonType {
+  className?: string;
+}
+
+console.log(isNumeric);
+
+const Button: React.FC<ButtonType> = ({ className }: ButtonType) => (
+  <button className={`ant-btn ant-btn-primary ${className ?? ''}`}>点我</button>
+);
+
+Button.defaultProps = {
+  className: '',
+};
+
+export default Button;
