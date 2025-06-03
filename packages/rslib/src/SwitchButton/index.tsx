5e92821626d8a0a59b54a6088344075d05537b7b
chengzao <czhlink@163.com>
2025-06-03 14:01:20 +0800

    refactor(SwitchButton): simplify component structure by removing container div

--- packages/rslib/src/SwitchButton/index.tsx
+++ packages/rslib/src/SwitchButton/index.tsx
@@ -9,36 +9,32 @@ const SwitchButton = () => {
   };
 
   return (
-    <div className="switch-container">
-      <div className="switch-wrapper">
-        <div
-          className={`switch-option ${
-            activeOption === 'jobSeeker' ? 'active' : ''
-          }`}
-          onClick={() => handleOptionChange('jobSeeker')}
-        >
-          Job Seeker
-        </div>
-
-        <div
-          className={`switch-option ${
-            activeOption === 'recruiter' ? 'active' : ''
-          }`}
-          onClick={() => handleOptionChange('recruiter')}
-        >
-          Recruiter
-        </div>
-
-        <div
-          className="switch-slider"
-          style={{
-            transform:
-              activeOption === 'jobSeeker'
-                ? 'translateX(0)'
-                : 'translateX(100%)',
-          }}
-        />
+    <div className="switch-wrapper">
+      <div
+        className={`switch-option ${
+          activeOption === 'jobSeeker' ? 'active' : ''
+        }`}
+        onClick={() => handleOptionChange('jobSeeker')}
+      >
+        JobSeeker
       </div>
+
+      <div
+        className={`switch-option ${
+          activeOption === 'recruiter' ? 'active' : ''
+        }`}
+        onClick={() => handleOptionChange('recruiter')}
+      >
+        Recruiter
+      </div>
+
+      <div
+        className="switch-slider"
+        style={{
+          transform:
+            activeOption === 'jobSeeker' ? 'translateX(0)' : 'translateX(100%)',
+        }}
+      />
     </div>
   );
 };
