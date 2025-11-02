2785787eb7c58c78b59d06cc2130663dca8f590d
chengzao <czhlink@163.com>
2025-11-02 19:52:21 +0800

    chore: update commitlint to enforce english commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'always', []], // 禁用subject-case规则
+  },
+};
